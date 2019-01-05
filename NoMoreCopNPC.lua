cd0f0e1111e6a9533cb600b05d77f7cb66ab2971
SphinxReal <46396830+SphinxReal@users.noreply.github.com>
2019-01-05 22:02:26 +0100

    Add files via upload

---
+++ NoMoreCopNPC.lua
@@ -0,0 +1,7 @@
+Citizen.CreateThread(function()
+    while true do
+        Citizen.Wait(0)
+        local myCoords = GetEntityCoords(GetPlayerPed(-1))
+        ClearAreaOfCops(myCoords.x, myCoords.y, myCoords.z, 100.0, 0)
+    end
+end)
